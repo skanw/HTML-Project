b964136bf27cd2b5373c3d6f67b778e44af049f1
Ade Oyefeso-Odusami <adeoyefeso1@gmail.com>
2022-07-25 15:03:40 -0400

    php changes

--- src/php/config.php
+++ src/php/config.php
@@ -1,3 +1,3 @@
 <?php
 
-//$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost', 'root', '', 'shop_db') or die('connection failed');
